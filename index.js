58b3a1adcff7d5ca4e69891219ad69892f0869ce
caden.reynolds <caden.reynolds@us.af.mil>
2022-01-23 21:26:46 -0700

    setting cookie maybe

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const corsOptions = {
 app.use(cors(corsOptions))
 app.use(bodyParser.json());
 app.use(
-    session({ secret: "Blaggo", resave: true, saveUninitialized: true, sameSite: 'none' })
+    session({ secret: "Blaggo", resave: true, saveUninitialized: true, cookie: {sameSite: 'none'}})
 );
 
 app.use('/user', user)
